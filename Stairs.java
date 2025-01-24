dc4cf4fe7d27f93d0c46763874a077f586b7bcc1
HussamShah10 <145381883+HussamShah10@users.noreply.github.com>
2025-01-24 10:42:47 -0500

    Update Stairs.java

--- Stairs.java
+++ Stairs.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class Stairs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter character: ");
